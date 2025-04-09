8310fa1940d0d2f062759e642a6642154b2839f6
Nwlcnf <153742803+Nwlcnf@users.noreply.github.com>
2025-04-09 15:36:46 +0200

    modif backend

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "ynov-nawel"
-    storage_account_name = "ynovstorage"
-    container_name       = "tfstatefiles"
-    key                  = "ynov1.tfstatefiles"
+    resource_group_name  = "ynov-web2"
+    storage_account_name = "ynovnawel"
+    container_name       = "tfstate"
+    key                  = "ynov1.tfstate"
     tenant_id            = "38e72bba-3c22-4382-9323-ac1612931297"
     subscription_id      = "dfdaec15-00ee-472c-b134-b4d3d6a6c05c"
   }
